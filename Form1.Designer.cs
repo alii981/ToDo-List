63de38b8038029a6ace7086f8217a9634077d463
alii981 <sandurobert90@yahoo.com>
2024-08-08 13:12:36 +0300

    ToDo Release

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace todo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.Add = new System.Windows.Forms.Button();
+            this.Remove = new System.Windows.Forms.Button();
+            this.Check = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            this.MoveUp = new System.Windows.Forms.Button();
+            this.MoveDown = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(348, 420);
+            this.listBox1.TabIndex = 0;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(421, 160);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(202, 20);
+            this.txtInput.TabIndex = 1;
+            this.txtInput.Text = " ";
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(672, 157);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 2;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Remove
+            // 
+            this.Remove.Location = new System.Drawing.Point(412, 234);
+            this.Remove.Name = "Remove";
+            this.Remove.Size = new System.Drawing.Size(75, 23);
+            this.Remove.TabIndex = 3;
+            this.Remove.Text = "Remove";
+            this.Remove.UseVisualStyleBackColor = true;
+            this.Remove.Click += new System.EventHandler(this.Remove_Click);
+            // 
+            // Check
+            // 
+            this.Check.Location = new System.Drawing.Point(672, 234);
+            this.Check.Name = "Check";
+            this.Check.Size = new System.Drawing.Size(75, 23);
+            this.Check.TabIndex = 4;
+            this.Check.Text = "Check";
+            this.Check.UseVisualStyleBackColor = true;
+            this.Check.Click += new System.EventHandler(this.Check_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(466, 346);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(75, 23);
+            this.Reset.TabIndex = 5;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // MoveUp
+            // 
+            this.MoveUp.Location = new System.Drawing.Point(421, 34);
+            this.MoveUp.Name = "MoveUp";
+            this.MoveUp.Size = new System.Drawing.Size(75, 23);
+            this.MoveUp.TabIndex = 6;
+            this.MoveUp.Text = "Move Up";
+            this.MoveUp.UseVisualStyleBackColor = true;
+            this.MoveUp.Click += new System.EventHandler(this.MoveUp_Click);
+            // 
+            // MoveDown
+            // 
+            this.MoveDown.Location = new System.Drawing.Point(421, 85);
+            this.MoveDown.Name = "MoveDown";
+            this.MoveDown.Size = new System.Drawing.Size(75, 23);
+            this.MoveDown.TabIndex = 7;
+            this.MoveDown.Text = "Move Down";
+            this.MoveDown.UseVisualStyleBackColor = true;
+            this.MoveDown.Click += new System.EventHandler(this.MoveDown_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MoveDown);
+            this.Controls.Add(this.MoveUp);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.Check);
+            this.Controls.Add(this.Remove);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Remove;
+        private System.Windows.Forms.Button Check;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Button MoveUp;
+        private System.Windows.Forms.Button MoveDown;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace todo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtInput.Text))
+            {
+                listBox1.Items.Add(txtInput.Text);
+                txtInput.Clear();
+            }
+        }
+
+        private void Remove_Click(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedIndex != -1)
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+        }
+
+        private void Check_Click(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedIndex != -1)
+            {
+                int index = listBox1.SelectedIndex;
+                string currentTask = listBox1.Items[index].ToString();
+                listBox1.Items[index] = currentTask + " (Completed)";
+            }
+        }
+
+        private void MoveUp_Click(object sender, EventArgs e)
+        {
+            MoveItem(-1);
+        }
+
+        private void MoveDown_Click(object sender, EventArgs e)
+        {
+            MoveItem(1);
+        }
+        private void MoveItem(int direction)
+        {
+            if (listBox1.SelectedItem == null || listBox1.SelectedIndex < 0)
+                return; // no item selected
+            int newIndex = listBox1.SelectedIndex + direction;
+            if (newIndex < 0 || newIndex >= listBox1.Items.Count)
+                return; // Index out of range
+            object selected = listBox1.SelectedItem;
+            listBox1.Items.Remove(selected);
+            listBox1.Items.Insert(newIndex, selected);
+            listBox1.SetSelected(newIndex, true);
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
